04deabad38f6763816365f9bcba606703cb21a59
Ivan Hrekov <iandrc9@gmail.com>
2025-10-07 11:19:33 +0200

    feat: complete exercises

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,14 @@
 'use strict';
 
+// This one is not optimal due to string re-allocations
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  const base = possible.length;
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * base);
+    key += possible[index];
+  }
+  return key;
 };
 
 module.exports = { generateKey };
